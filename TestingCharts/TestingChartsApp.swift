8a67ea2019b3c162ffa4c0a02313e0c83c04a5af
Oscar Epp <oepp@uoregon.edu>
2024-05-09 14:29:13 -0700

    Initial Commit

---
+++ TestingCharts/TestingChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingChartsApp.swift
+//  TestingCharts
+//
+//  Created by Oscar Epp on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
